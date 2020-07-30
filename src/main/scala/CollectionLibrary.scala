bd4fe6c795cb9b64dad661a8e2b43525c36552a1
buntafujikawa <bunta.fujikawa@gmail.com>
2020-07-30 21:40:59 +0900

    add collection library

--- src/main/scala/CollectionLibrary.scala
+++ src/main/scala/CollectionLibrary.scala
@@ -65,4 +65,112 @@ object CollectionLibrary {
   def mul(list: List[Int]): Int = {
     list.foldRight(1)((x, y) => x * y)
   }
+
+  def mkString[T](list: List[T]) (sep: String): String = list match {
+    case Nil => ""
+    case x::xs => xs.foldLeft(x.toString()){(x, y) => x + sep + y}
+  }
+
+  def mkString[T](list: List[T])(sep: String): String = list match {
+    case Nil => ""
+    case x::xs => xs.foldLeft(x.toString){(x, y) => x + sep + y}
+  }
+
+  List(1,2,3,4,5).map(x => x * 2)
+
+  def map[T, U](list: List[T])(f: T => U): List[U] = {
+    list.foldLeft(Nil:List[U]){(x,y) => f(y) :: x}.reverse
+  }
+  def map2[T, U](list: List[T])(f: T => U): List[U] = {
+    list.foldLeft(Nil:List[U]){(x,y) => f(y) :: x}
+  }
+  assert(List(2,3,4) == map(List(1,2,3))(x => x + 1))
+
+  def filter[T](list: List[T])(f: T => Boolean): List[T] = {
+    list.foldLeft(Nil:List[T]){(x,y) => if(f(y)) y::x else x}.reverse
+  }
+
+  def find[T](list: List[T])(f: T => Boolean): Option[T] = list match {
+    case x::xs if f(x) => Some(x)
+    case x::xs => find(xs)(f)
+    case _ => None
+  }
+
+  def find[T](list: List[T])(f: T => Boolean): Option[T] = {
+    list.foldLeft(None){(x, y) =>
+      if (f(y)) return Some(y)
+      None
+    }
+  }
+  assert(Some(2) == find(List(1, 2, 3))(x => x == 2))
+  assert(None == find(List(1, 2, 3))(x => x > 3))
+  assert(Some(1) == find(List(1))(x => x == 1))
+  assert(None == find(List(1))(x => false))
+  assert(None == find(List[Int]())(x => x == 1))
+
+  def takeWhile[T](list: List[T])(f: T => Boolean): List[T] = {
+    list.foldLeft(Nil:List[T]){(x, y) => if(f(y)) y::x else x }.reverse
+  }
+  assert(List(1, 2, 3) == takeWhile(List(1, 2, 3, 4, 5))(x => x <= 3))
+  assert(List(1) == takeWhile(List(1, 2, 3, 3, 4, 5))(x => x == 1))
+  assert(List(1, 2, 3, 4)  == takeWhile(List(1, 2, 3, 4, 5))(x => x < 5))
+  assert(Nil == takeWhile(List(1, 2, 3, 3, 2, 2))(x => false))
+
+  def count[T](list: List[T])(f: T => Boolean): Int = {
+    list.foldLeft(0){(x,y) => if(f(y)) x + 1 else x }
+  }
+  assert(3 == count(List(1, 2, 3, 3, 2, 2))(x => x == 2))
+  assert(1 == count(List(1, 2, 3, 3, 2, 2))(x => x == 1))
+  assert(2 == count(List(1, 2, 3, 3, 2, 2))(x => x == 3))
+  assert(0 == count(List(1, 2, 3, 3, 2, 2))(x => x == 5))
+
+  def flatMap[T, U](list: List[T])(f: T => List[U]): List[U] = list match {
+    case Nil => Nil
+    case x::xs => f(x) ::: flatMap(xs)(f)
+  }
+  assert(List(1, 2, 3) == flatMap(List(1, 2, 3))(x => List(x)))
+  assert(
+    List(3, 4, 6, 8) == flatMap(List(1, 2))(x =>
+      map(List(3, 4))(y => x * y)
+    )
+  )
+
+  5 :: List(1, 2, 3, 4)
+  List(1, 2, 3, 4) :+ 5 // 末尾への追加は、Listの要素数分かかる
+
+  /*
+  # vector
+  Vectorは一度データ構造を構築したら変更できないimmutableなデータ構造
+   */
+  Vector(1, 2, 3, 4, 5) //どの操作も「ほぼ」一定の時間で終わる
+  6 +: Vector(1, 2, 3, 4, 5)
+  Vector(1, 2, 3, 4, 5) :+ 6
+  Vector(1, 2, 3, 4, 5).updated(2, 5) // Vector(1, 2, 5, 4, 5)
+
+  /*
+  # Map (連想配列)
+  Mapと書いた場合、scala.collection.immutable.Mapが使われます。その名の通り、一度作成すると変更することはできません
+   */
+  val m = Map("A" -> 1, "B" -> 2, "C" -> 3)
+  m.updated("B", 4)
+  m // そのまま
+
+  import scala.collection.mutable
+
+  val mutableMap = mutable.Map("A" -> 1, "B" -> 2, "C" -> 3)
+  mutableMap("B") = 5
+  m
+
+  /*
+  # Set
+  値の集合を提供するデータ構造です。Setの中では同じ値が2つ以上存在しません
+   */
+
+  val s = Set(1, 1, 2, 3, 4) // Set(1, 2, 3, 4)
+  s - 4
+  s
+
+  val mutableSet = mutable.Set(1, 2, 3, 4, 5)
+  mutableSet -= 5
+  mutableSet
 }
